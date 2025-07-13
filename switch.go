6379c11dd465d25bf3245d527d0436539e36a433
Rafael Pandu <rafaelsumanti01@gmail.com>
2025-07-13 17:46:58 +0700

    add switch

---
+++ switch.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "random"
+
+	switch name {
+	case "ravel":
+		fmt.Println("Hai Admin")
+	case "zeta":
+		fmt.Println("hai pacar rapel")
+	default:
+		fmt.Println("siapa lu")
+	}
+}
